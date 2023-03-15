cadee5c0617429a857ae0f2228ecbef436da9f13
DMartin <davidmartin8127@gmail.com>
2023-03-15 13:21:57 -0700

    added daisy ui and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from './components/Header';
+import Nav from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <span>Hello World</span>
+    <div className="App">
+      <Nav />
+      <div className="w-screen h-screen flex items-center justify-center">
+        <Header />
+        <span>Hello World</span>
+
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div>Header</div>
+      <button className='btn btn-sm'>click</button>
+    </>
+  )
+}
+
+export default Header;
